480d59a8e8fe3cbe3ff01d883822be56fce249d9
RobertCurry <61264063+RobertCurryTTW@users.noreply.github.com>
2020-09-01 20:19:23 +1000

    init commit, upto chapter 1.4

---
+++ token/token.go
@@ -0,0 +1,58 @@
+package token
+
+//TokenType is a type of token
+type Type string
+
+//Token is a token
+type Token struct {
+	Type    Type
+	Literal string
+}
+
+//New => Token constructor
+func New(tokenType Type, ch byte) Token {
+	return Token{Type: tokenType, Literal: string(ch)}
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	//Identifiers + literals
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	//Delimiters
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]Type{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+//LookupIdent cheks if identifier is a keyword
+func LookupIdent(ident string) Type {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
